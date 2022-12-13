7062322d8653208592ea57140f1d6c45f267459e
JonathanKBP <jonathan.kevinbp@hotmail.com>
2022-12-13 08:43:46 -0300

    Aula: Modo fluente

--- src/main/java/br/ce/jonathankbp/rest/olaMundoTest.java
+++ src/main/java/br/ce/jonathankbp/rest/olaMundoTest.java
@@ -1,5 +1,7 @@
 package br.ce.jonathankbp.rest;
 
+import static io.restassured.RestAssured.*;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,14 +14,29 @@ public class olaMundoTest {
 	
 	@Test
 	public void testOlaMundo() {
-		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
 		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
 		Assert.assertTrue(response.statusCode() == 200);
 		Assert.assertTrue("O status code deveria ser 200", response.statusCode() == 200);
-		Assert.assertEquals(201, response.statusCode());
+		Assert.assertEquals(200, response.statusCode());
 		
 		ValidatableResponse validacao = response.then();
 		validacao.statusCode(200);
-
+	}
+	
+	@Test
+	public void devoConhecerOutrasFormasRestAssured() {
+		Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");	
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		
+		get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+		
+		given() //Pré condições		
+		.when() //Ação
+			.get("http://restapi.wcaquino.me/ola")
+		.then() //Assertivas
+			//.assertThat();
+			.statusCode(200);
 	}
 }
