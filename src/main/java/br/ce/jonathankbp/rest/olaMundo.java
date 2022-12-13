17595af3d3162f1cfd974d0446b09dc613002c7b
JonathanKBP <jonathan.kevinbp@hotmail.com>
2022-12-13 01:28:41 -0300

    Aula: JUnit

--- src/main/java/br/ce/jonathankbp/rest/olaMundo.java
+++ src/main/java/br/ce/jonathankbp/rest/olaMundo.java
@@ -3,12 +3,16 @@ package br.ce.jonathankbp.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class olaMundo {
 	
 	public static void main(String[] args) {
 		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
-
 		System.out.println(response.getBody().asString());
+		System.out.println(response.statusCode() == 200);
+		
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(201);
 	}
 }
