d9e76e64abbb0e29bd6e810841385e158dc81d8a
Jonathan Pinto <jonathan.kevinbp@hotmail.com>
2022-12-27 01:07:46 -0300

    Aula: inserindo com POST

---
+++ src/main/java/br/ce/jonathankbp/rest/VerbosTest.java
@@ -0,0 +1,27 @@
+package br.ce.jonathankbp.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+public class VerbosTest {
+
+	@Test
+	public void deveSalvarUsuario() {
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body("{\"name\": \"jose\", \"age\": 50}")
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("jose"))
+			.body("age", is(50));
+	}
+	
+}
+
