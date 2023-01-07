dee2678ff888aed43158f4e5f4bd1a200d054ada
Jonathan Pinto <jonathan.kevinbp@hotmail.com>
2023-01-06 22:25:20 -0300

    Aula: Query

---
+++ src/main/java/br/ce/jonathankbp/rest/EnvioDadosTest.java
@@ -0,0 +1,23 @@
+package br.ce.jonathankbp.rest;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+
+public class EnvioDadosTest {
+	
+	@Test
+	public void deveEnviarValorViaQuery() {
+		given()
+			.log().all()
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users?format=xml")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML);
+	}
+
+}
