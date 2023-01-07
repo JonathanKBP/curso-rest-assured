50a07140a1b55298d51f814d5b1c9dd28b37336f
Jonathan Pinto <jonathan.kevinbp@hotmail.com>
2023-01-06 23:38:51 -0300

    Aula: Header

--- src/main/java/br/ce/jonathankbp/rest/EnvioDadosTest.java
+++ src/main/java/br/ce/jonathankbp/rest/EnvioDadosTest.java
@@ -35,5 +35,17 @@ public class EnvioDadosTest {
 			.contentType(containsString("utf-8"));
 	}
 
+	@Test
+	public void deveEnviarValorViaHeader() {
+		given()
+			.log().all()
+			.accept(ContentType.HTML)
+		.when()
+			.get("https://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.HTML);
+	}
 
 }
