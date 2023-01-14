43f69b500599eb606a121159c0b657f05b49c3e9
Jonathan Pinto <jonathan.kevinbp@hotmail.com>
2023-01-13 23:01:09 -0300

    Aula: Acessando API com chave

--- src/main/java/br/ce/jonathankbp/rest/AuthTest.java
+++ src/main/java/br/ce/jonathankbp/rest/AuthTest.java
@@ -18,4 +18,23 @@ public class AuthTest {
 			.statusCode(200)
 			.body("name", is("Luke Skywalker"));
 	}
+	
+	@Test
+	public void deveObterClima() {
+		given()
+			.log().all()
+			.queryParam("q", "Fortaleza,BR")
+			.queryParam("appid", "6a008edace200193a30754d730d98bfd")
+		.when()
+			.get("https://api.openweathermap.org/data/2.5/weather")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("Fortaleza"))
+			.body("coord.lon", is(-38.52f))
+			.body("main.temp", greaterThan(25f));
+	}
 }
+
+
+
